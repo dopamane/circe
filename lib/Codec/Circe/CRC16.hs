77891efed7c56c2bc42b03463249e5d27d45eff1
dopamane <dwc1295@gmail.com>
2026-08-09 23:44:38 -0700

    cleanup

--- lib/Codec/Circe/CRC16.hs
+++ lib/Codec/Circe/CRC16.hs
@@ -10,14 +10,16 @@ import qualified Data.ByteString.Lazy as BS
 import Data.Vector.Storable (Vector)
 import qualified Data.Vector.Storable as V
 import Data.Function
+import Data.Monoid
+import Data.Semigroup
 import Data.Word
 
 -- | generate lookup table using polynomial
 crc16Table :: Word16 -> [Word16]
-crc16Table poly = [foldr go (dividend `shiftL` 8) [0..7] | dividend <- [0..255]]
+crc16Table poly = [appEndo e $ dividend `shiftL` 8 | dividend <- [0..255]]
   where
-    go :: Word8 -> Word16 -> Word16
-    go _ curByte = applyWhen (testBit curByte 15) (`xor` poly) $ curByte `shiftL` 1
+    e :: Endo Word16
+    e = stimes (8 :: Int) $ Endo $ \curByte -> applyWhen (testBit curByte 15) (`xor` poly) $ curByte `shiftL` 1
 
 -- | calculate CRC16 using an efficient lookup table and init value
 crc16 :: Vector Word16 -> Word16 -> ByteString -> Word16
