dc0b8a9021f484049aa780c350150b4dc4114fca
dopamane <dwc1295@gmail.com>
2026-08-09 20:19:17 -0700

    debug

--- lib/Codec/Circe/CRC16.hs
+++ lib/Codec/Circe/CRC16.hs
@@ -1,12 +1,18 @@
 module Codec.Circe.CRC16
   ( tableCRC16
+  , showW16
   ) where
 
 import Data.Bits
 import Data.Function
 import Data.Word
+import Numeric
 
 tableCRC16 :: Word16 -> [Word16]
 tableCRC16 poly = [foldr go (dividend `shiftL` 8) [0..7] | dividend <- [0..255]]
   where
-    go curByte _ = applyWhen (curByte .&. 0x8000 /= 0) (`xor` poly) $ curByte `shiftL` 1
+    go :: Word8 -> Word16 -> Word16
+    go _ curByte = applyWhen (testBit curByte 15) (`xor` poly) $ curByte `shiftL` 1
+
+showW16 :: Word16 -> String
+showW16 w16 = showHex w16 ""
