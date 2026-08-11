2287eeb534671336d942de9692b4e6b223e0cb7b
dopamane <dwc1295@gmail.com>
2026-08-11 01:42:21 -0700

    add high-level crc16 test

--- test/Test/Circe/CRC16.hs
+++ test/Test/Circe/CRC16.hs
@@ -3,6 +3,7 @@
 module Test.Circe.CRC16 (tests) where
 
 import Codec.Circe.CRC16
+import Codec.Circe.Cfg
 --import Data.Vector.Storable (Vector)
 import qualified Data.Vector.Storable as V
 import Test.Tasty
@@ -10,11 +11,17 @@ import Test.Tasty.HUnit
 
 tests :: TestTree
 tests = testGroup "CRC16"
-  [ basicTest
+  [ crc16UnsafeTest
+  , crc16Test
   ]
 
-basicTest :: TestTree
-basicTest = testCase "crc16Unsafe" $
+crc16UnsafeTest :: TestTree
+crc16UnsafeTest = testCase "crc16Unsafe" $
   crc16Unsafe (V.fromList t) 0 "bosscoxwuzhere" @?= 0xbe42
   where
     t = crc16Table 0x1021
+
+crc16Test :: TestTree
+crc16Test = testCaseSteps "crc16" $ \step -> do
+  step "modbus"
+  crc16 crc16Modbus "hello world" @?= 0xddc7
