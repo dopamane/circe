8de27fa5d3bc0f0e860104652c01bc31355739b3
dopamane <dwc1295@gmail.com>
2026-08-12 01:51:37 -0700

    add crc32, test

--- lib/Codec/Circe/CRC32.hs
+++ lib/Codec/Circe/CRC32.hs
@@ -1,9 +1,13 @@
 -- | CRC32 utilities
 module Codec.Circe.CRC32
-  ( crc32Unsafe
+  ( crc32
+  , crc32WithCfg
+  , crc32Unsafe
   , crc32Table
   ) where
 
+import Codec.Circe.Cfg
+import Codec.Circe.Reflect
 import Data.Bits
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as BS
@@ -13,27 +17,29 @@ import Data.Function
 import Data.Monoid
 import Data.Semigroup
 import Data.Word
-{-
--- | calculate CRC32 using lookup table generated from config
-crc16 :: CRC16Cfg -> ByteString -> Word16
-crc16 cfg = crc16WithCfg t cfg
-  where
-    t = V.fromList $ crc16Table $ crcPoly cfg
 
--- | calculate CRC16 with extra configuration, table not calculated from poly.
-crc16WithCfg :: Vector Word16 -> CRC16Cfg -> ByteString -> Word16
-crc16WithCfg t cfg =
+-- | calculate CRC32 using lookup table generated from config
+crc32 :: CRC32Cfg -> ByteString -> Word32
+crc32 cfg = crc32WithCfg t cfg
+  where
+    t = V.fromList $ crc32Table $ crcPoly cfg
+
+-- | calculate CRC32 with extra configuration, table not calculated from poly.
+crc32WithCfg :: Vector Word32 -> CRC32Cfg -> ByteString -> Word32
+crc32WithCfg t cfg =
   xor (crcFinXor cfg)
-    . applyWhen (crcRefOut cfg) ref16
-    . crc16Unsafe t (crcRefIn cfg) (crcInit cfg)
--}
+    . applyWhen (crcRefOut cfg) ref32
+    . crc32Unsafe t (crcRefIn cfg) (crcInit cfg)
+
 -- | calculate CRC32 using an efficient lookup table and init value
-crc32Unsafe :: Vector Word32 -> Word32 -> ByteString -> Word32
-crc32Unsafe t = BS.foldl' go
+crc32Unsafe :: Vector Word32 -> Bool -> Word32 -> ByteString -> Word32
+crc32Unsafe t refIn = BS.foldl' go
   where
     go crc b = crc `shiftL` 8 `xor` t `V.unsafeIndex` fromIntegral pos
       where
-        pos = fromIntegral ((crc `xor` (fromIntegral b `shiftL` 24)) `shiftR` 24) :: Word8
+        pos = fromIntegral ((crc `xor` (fromIntegral b' `shiftL` 24)) `shiftR` 24) :: Word8
+          where
+            b' = applyWhen refIn ref8 b
 
 -- | generate lookup table using polynomial
 crc32Table :: Word32 -> [Word32]
--- test/Test/Circe/CRC32.hs
+++ test/Test/Circe/CRC32.hs
@@ -3,6 +3,7 @@
 module Test.Circe.CRC32 (tests) where
 
 import Codec.Circe.CRC32
+import Codec.Circe.Cfg
 import qualified Data.Vector.Storable as V
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -10,10 +11,15 @@ import Test.Tasty.HUnit
 tests :: TestTree
 tests = testGroup "CRC32"
   [ crc32UnsafeTest
+  , crc32Test
   ]
 
 crc32UnsafeTest :: TestTree
 crc32UnsafeTest = testCase "crc32Unsafe" $
-  crc32Unsafe (V.fromList t) 0 "hello world" @?= 0x737af2ae
+  crc32Unsafe (V.fromList t) False 0 "hello world" @?= 0x737af2ae
   where
     t = crc32Table 0x4c11db7
+
+crc32Test :: TestTree
+crc32Test = testCase "crc32" $
+  crc32 crc32IEEE "bo$$ cox rulez" @?= 0x75ce60a3
